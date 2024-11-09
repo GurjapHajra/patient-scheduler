a794859c33b1d5bb6f2548a143857f75d1807be1
FabienThich <fabienthich30@gmail.com>
2024-11-09 18:04:45 -0500

    Update nav

--- too-cool-to-die/src/Components/Nav/nav.js
+++ too-cool-to-die/src/Components/Nav/nav.js
@@ -15,7 +15,7 @@ class my_navbar extends React.Component {
           <Navbar.Collapse>
             <Nav>
               <ul>
-                <li className="nav_li">
+                <li className="nav_li nav_li_left">
                   <NavLink to="/" className="navlink">
                     Home
                   </NavLink>
@@ -30,12 +30,12 @@ class my_navbar extends React.Component {
                     Professional
                   </NavLink>
                 </li>
-                <li className="nav_li">
+                <li className="nav_li nav_li_right">
                   <NavLink to="/signup-professional" className="navlink">
                     Finder
                   </NavLink>
                 </li>
-                <li className="nav_right">
+                <li className="nav_right nav_li_left nav_li_right">
                   <NavLink to="/login" className="navlink">
                     Log In
                   </NavLink>
