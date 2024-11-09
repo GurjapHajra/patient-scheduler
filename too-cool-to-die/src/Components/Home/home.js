a49008898ed4f00c3afe4bef97ef228a81619012
Daniel <daniel.lee.cont@gmail.com>
2024-11-09 17:23:55 -0500

    worked on home page

--- too-cool-to-die/src/Components/Home/home.js
+++ too-cool-to-die/src/Components/Home/home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import logo from "./assets/logo.png"
 import "./home.css";
 
 function Home() {
   return (
     <>
-      Hello
+      <div className= "background" alt = "TCTD">
+        <div className="mainContainer">
+          <img src={logo} className="Logo" style={{ width: '50%', height: 'auto', marginTop: '10px' }} alt="logo"/>
+          <div className = "buttonContainer">
+            <button className="button" style={{backgroundColor: "#008080"}}>Patient</button>
+            <button className="button" style={{backgroundColor: "#373130"}}>Professional</button>
+          </div>
+        </div>
+      </div> 
     </>
   );
 }
