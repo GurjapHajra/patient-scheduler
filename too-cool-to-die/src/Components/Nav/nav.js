ba8a3953b393b71d4135404bc19660bc204c549d
FabienThich <fabienthich30@gmail.com>
2024-11-09 16:10:49 -0500

    Add folders

---
+++ too-cool-to-die/src/Components/Nav/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./nav.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar } from "react-bootstrap";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    );
+  }
+}
+
+export default Header;
