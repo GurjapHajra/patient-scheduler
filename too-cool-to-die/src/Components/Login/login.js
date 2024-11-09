6dc3d60c0edc5bcdaa20e33b49bccd73d49f0e7e
Daniel <daniel.lee.cont@gmail.com>
2024-11-09 18:14:10 -0500

    login page

--- too-cool-to-die/src/Components/Login/login.js
+++ too-cool-to-die/src/Components/Login/login.js
@@ -1,7 +1,20 @@
+import "./login.css"
 function Login() {
     return(
         <>
-        Login form here
+        <div className="login-box">
+            <h2>Login</h2>
+            <form>
+                <div className="user-box">
+                    <input type="text" name="" required=""/>
+                    <label>Username</label>
+                </div>
+                <div className="user-box">
+                    <input type="password" name="" required=""/>
+                    <label>Password</label>
+                </div>
+            </form>
+        </div>
         </>
     )
 }
