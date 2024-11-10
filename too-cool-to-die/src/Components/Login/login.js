14eb1e7a54bd57431627a7abd47b2527fa7bea11
FabienThich <fabienthich30@gmail.com>
2024-11-09 19:08:09 -0500

    Update

--- too-cool-to-die/src/Components/Login/login.js
+++ too-cool-to-die/src/Components/Login/login.js
@@ -1,23 +1,22 @@
-import "./login.css"
+import "./login.css";
 function Login() {
-    return(
-        <>
-        <div className="login-box">
-            <h2>Login</h2>
-            <form>
-                <div className="user-box">
-                    <input type="text" name="" required=""/>
-                    <label>Username</label>
-                </div>
-                <div className="user-box">
-                    <input type="password" name="" required=""/>
-                    <label>Password</label>
-                </div>
-            </form>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="login-box">
+        <h2>Login</h2>
+        <form>
+          <div className="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name="" required="" />
+            <label>Password</label>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-
-export default Login
+export default Login;
