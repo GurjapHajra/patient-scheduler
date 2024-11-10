a1ebd36840a492efcf38061210f27bbf4e65e62d
FabienThich <fabienthich30@gmail.com>
2024-11-09 20:09:22 -0500

    Add patient form

--- too-cool-to-die/src/Components/SignUp/signup-patient.js
+++ too-cool-to-die/src/Components/SignUp/signup-patient.js
@@ -1,11 +1,80 @@
-import "./signup.css";
-function signup_patient() {
-    return(
-        <>
-            <div className="background">
+function SignupPatient() {
+  return (
+    <>
+      <div className="backgroundImg">
+        <div className="signup-box mt-5">
+          <h2>New Patient Application</h2>
+          <form>
+            <div className="input-row">
+              <div className="user-box-2">
+                <input type="text" name="firstName" required />
+                <label>Username</label>
+              </div>
+              <div className="user-box-2">
+                <input type="password" name="password" required />
+                <label>Password</label>
+              </div>
             </div>
-        </>
-    )
+            <div className="input-row">
+              <div className="user-box-2">
+                <input type="text" name="firstName" required />
+                <label>First Name</label>
+              </div>
+              <div className="user-box-2">
+                <input type="text" name="lastName" required />
+                <label>Last Name</label>
+              </div>
+              <div className="user-box-2">
+                <label className="gender-label"></label>
+                <select name="gender" required>
+                  <option value="">Select Sex</option>
+                  <option className="dropdown-color" value="male">
+                    Male
+                  </option>
+                  <option className="dropdown-color" value="female">
+                    Female
+                  </option>
+                  <option className="dropdown-color" value="other">
+                    Other
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            <div className="input-row">
+              <div className="user-box-2">
+                <input type="text" name="age" required />
+                <label>Age</label>
+              </div>
+              <div className="user-box-2">
+                <input type="text" name="weight" required />
+                <label>Weight (lb)</label>
+              </div>
+              <div className="user-box-2">
+                <input type="text" name="height" required />
+                <label>Height (cm)</label>
+              </div>
+            </div>
+
+            <div className="input-row">
+              <div className="user-box-2">
+                <label htmlFor="description"></label>
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Medical History (Text box): Allergies & Previous diagnoses/treatments"
+                ></textarea>
+              </div>
+            </div>
+
+            <button className="button" style={{ backgroundColor: "#373130" }}>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default signup_patient;
+export default SignupPatient;
