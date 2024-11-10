49f5ebfa28fa97ad6e310f4d7dd93db61ff86a20
FabienThich <fabienthich30@gmail.com>
2024-11-09 19:19:26 -0500

    add button:

--- too-cool-to-die/src/Components/Login/login.js
+++ too-cool-to-die/src/Components/Login/login.js
@@ -1,24 +1,28 @@
 import "./login.css";
 function Login() {
-    return(
-        <>
-        <div className = "backgroundImg">
-            <div className="login-box">
-                <h2>Login</h2>
-                <form>
-                    <div className="user-box">
-                        <input type="text" name="" required=""/>
-                        <label>Username</label>
-                    </div>
-                    <div className="user-box">
-                        <input type="password" name="" required=""/>
-                        <label>Password</label>
-                    </div>
-                </form>
+  return (
+    <>
+      <div className="backgroundImg">
+        <div className="login-box">
+          <h2>Login</h2>
+          <form>
+            <div className="user-box">
+              <input type="text" name="" required="" />
+              <label>Username</label>
             </div>
+            <div className="user-box">
+              <input type="password" name="" required="" />
+              <label>Password</label>
+            </div>
+
+            <button className="button" style={{ backgroundColor: "#373130" }}>
+              Log In
+            </button>
+          </form>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Login;
