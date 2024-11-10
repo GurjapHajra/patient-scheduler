dd73c1e25395fb3c5b6ff1cea87efaec8f0b599c
FabienThich <fabienthich30@gmail.com>
2024-11-10 12:53:53 -0500

    new api

--- too-cool-to-die/src/Components/Finder/finder.js
+++ too-cool-to-die/src/Components/Finder/finder.js
@@ -7,6 +7,12 @@ function Finder() {
         style={{ height: "100%", minHeight: "550px", marginTop: "20vh" }}
         frameborder="0"
       ></iframe>
+      {/* <iframe
+        src="https://www.chatbase.co/chatbot-iframe/dfqBuZfTuj2ho2LDDFFp1"
+        width="100%"
+        style="height: 100%; min-height: 700px"
+        frameborder="0"
+      ></iframe> */}
     </>
   );
 }
